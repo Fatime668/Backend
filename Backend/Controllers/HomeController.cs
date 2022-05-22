8d6db38fb0e08fd8b09a4ae6436824874f44d7ef
Fatime668 <tu7dkjdue@code.edu.az>
2022-05-23 00:10:24 +0400

    Back

---
+++ Backend/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index(int id) 
+        {
+            //return Content(id.ToString());
+            ViewBag.Product = "Cola";
+            ViewData["Count"] = id;
+            TempData["Type"] = "Cold";
+            if (id == 2)
+            {
+                return RedirectToAction("About");
+            }
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult FAQ(string name,string surname)
+        {
+            string fullname = $"{name} {surname}";
+            return Content(fullname);
+        }
+    }
+}
